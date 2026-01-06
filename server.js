c4129ea9c6cae0559f2e4a60d32d577a581b7aca
manduakash <manduakash@gmail.com>
2026-01-06 14:10:26 +0530

    main

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ import { fileURLToPath } from "url";
 import { data as renting_single_declaration } from "./dummy-data/renting/normal/single/renting_single_declaration.js";
 import { data as renting_single_final } from "./dummy-data/renting/normal/single/renting_single_final.js";
 import { data as renting_single_provisional } from "./dummy-data/renting/normal/single/renting_single_provisional.js";
+import { data as renting_multi_owner_declaration } from "./dummy-data/renting/normal/multi/renting_multi_owner_declaration.js";
+import { data as renting_multi_owner_provisional } from "./dummy-data/renting/normal/multi/renting_multi_owner_provisional.js";
+import { data as renting_multi_owner_single_declaration } from "./dummy-data/renting/normal/multi/renting_multi_owner_single_declaration.js";
+import { data as renting_multi_owner_final } from "./dummy-data/renting/normal/multi/renting_multi_owner_final.js";
 dotenv.config();
 
 const app = express();
@@ -20,16 +24,29 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // API to show the certificate page
+// Renting Single Party
 app.get("/renting-single-declaration", (req, res) => {
     res.render("renting/normal/single/renting-single-declaration", { data: renting_single_declaration });
 });
-
 app.get("/renting-single-final", (req, res) => {
     res.render("renting/normal/single/renting-single-final", { data: renting_single_final });
 });
-
 app.get("/renting-single-provisional", (req, res) => {
     res.render("renting/normal/single/renting-single-provisional", { data: renting_single_provisional });
+});
+
+// Renting Multi Party
+app.get("/renting-multi-owner-declaration", (req, res) => {
+    res.render("renting/normal/multi/renting-multi-owner-declaration", { data: renting_multi_owner_declaration });
+});
+app.get("/renting-multi-owner-single-declaration", (req, res) => {
+    res.render("renting/normal/multi/renting-multi-owner-single-declaration", { data: renting_multi_owner_single_declaration });
+});
+app.get("/renting-multi-owner-provisional", (req, res) => {
+    res.render("renting/normal/multi/renting-multi-owner-provisional", { data: renting_multi_owner_provisional });
+});
+app.get("/renting-multi-owner-final", (req, res) => {
+    res.render("renting/normal/multi/renting-multi-owner-final", { data: renting_multi_owner_final });
 });
 
 // Root redirect
